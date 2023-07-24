91314c4c85e99e43b3a646e90dcbec884d4fd82c
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-07-24 19:03:33 +0200

    Update App.jsx

--- phonebook-reactjs-goit-hw-08/src/components/App.jsx
+++ phonebook-reactjs-goit-hw-08/src/components/App.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import SharedLayout from './SharedLayout/SharedLayout';
-import { Suspense, lazy, useEffect } from 'react';
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
-import { useDispatch } from 'react-redux';
-import { refreshUser } from '../redux/auth/operations';
-import { useAuth } from '../hooks/useAuth';
-import Loader from './Loader/Loader';
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
-const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
-const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+import { Route, Routes, Navigate } from "react-router-dom";
+import SharedLayout from "./SharedLayout/SharedLayout";
+import { Suspense, lazy, useEffect } from "react";
+import PublicRoute from "./PublicRoute";
+import PrivateRoute from "./PrivateRoute";
+import { useDispatch } from "react-redux";
+import { refreshUser } from "../redux/auth/operations";
+import { useAuth } from "../hooks/useAuth";
+import Loader from "./Loader/Loader";
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage/RegisterPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("../pages/ContactsPage/ContactsPage"));
+const NotFound = lazy(() => import("../pages/NotFound/NotFound"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,21 +33,27 @@ const App = () => {
             path="/register"
             element={
               <PublicRoute
-                redirectTo={<Navigate to="/contacts" replace />}
                 component={<RegisterPage />}
+                redirectTo={<Navigate to="/contacts" replace />}
               />
             }
           />
           <Route
             path="/login"
             element={
-              <PublicRoute redirectTo={<Navigate to="/contacts" replace />} component={<LoginPage />} />
+              <PublicRoute
+                redirectTo={<Navigate to="/contacts" replace />}
+                component={<LoginPage />}
+              />
             }
           />
           <Route
             path="/contacts"
             element={
-              <PrivateRoute redirectTo={<Navigate to="/login" replace />} component={<ContactsPage />} />
+              <PrivateRoute
+                redirectTo={<Navigate to="/login" replace />}
+                component={<ContactsPage />}
+              />
             }
           />
         </Route>
